62bfb6f2e02d51cbe9825bb5c424d002d0d3863e
Alessandro Marrella <dev@alessandromarrella.com>
2020-09-05 10:22:38 +0100

    nix maintenance

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
 let
   sources = import ./nix/sources.nix;
   haskellNix = import sources.haskell-nix {};
-  nixpkgsSrc = haskellNix.sources.nixpkgs-1909;
+  nixpkgsSrc = haskellNix.sources.nixpkgs-2003;
   nixpkgsArgs = haskellNix.nixpkgsArgs;
 in
 { pkgs ? import nixpkgsSrc nixpkgsArgs
 }:
-  pkgs.haskell-nix.stackProject {
-    src = ./src;
+  pkgs.haskell-nix.project {
+    src = pkgs.haskell-nix.haskellLib.cleanGit {
+      name = "haskell-nix-project";
+      src = ./src;
+    };
+    compiler-nix-name = "ghc884";
   }
